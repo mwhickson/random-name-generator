2666663d212bf516b9281de46600dea1e119456f
Matthew Hickson <matthewhickson@outlook.com>
2025-06-19 21:29:05 -0400

    converting to Pascal for fun and profit!

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+run bin scripts from project root
---
+++ src/namegen.pas
@@ -0,0 +1,10 @@
+{$ifdef FPC} {$mode objfpc}{$H+}{$J-} {$endif}
+{$ifdef MSWINDOWS} {$apptype CONSOLE} {$endif}
+{$R+}
+
+program namegen;
+
+begin
+	writeln('*** Random Name Generator ***');
+end.
+

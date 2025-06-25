2429db63dbe79a87d09d97d504e1689a0a15c423
Matthew Hickson <matthewhickson@outlook.com>
2025-06-24 22:26:52 -0400

    simple name generation restored

--- namegen.py
+++ namegen.py
@@ -5,6 +5,7 @@
 #
 
 import os
+import random
 
 DefaultNameDataFile: str = "./data/names.txt"
 
@@ -18,19 +19,41 @@ class NameGenerator:
 	def get_name_data(filename: str) -> list[str]:
 		if os.path.exists(filename):
 			f = open(filename, "rt")
-			lines = f.readlines()
+			lines = f.read()
 			f.close()
-			return lines
+			return lines.splitlines()
 		else:
 			return []
 
-	def get_name(self, name_segment_array: list[str], starts_with: str, minimum_length: int, maximum_length: int) -> str:
-		pass
+	@staticmethod
+	def get_name(name_segment_array: list[str], starts_with: str, minimum_length: int, maximum_length: int) -> str:
+		name = ""
+		name_length = random.randint(minimum_length, maximum_length)
 
-	def get_names(self, name_segment_array: list[str], starts_with: str, minimum_length: int, maximum_length: int, count: int) -> list[str]:
-		pass
+		while len(name) <=  name_length:
+			selected_segment = name_segment_array[random.randint(0, len(name_segment_array) - 1)]
+			name = name + selected_segment
+
+		if len(name) > name_length:
+			name = name[:name_length].capitalize()
+
+		return name
+
+	@staticmethod
+	def get_names(name_segment_array: list[str], starts_with: str, minimum_length: int, maximum_length: int, count: int) -> list[str]:
+		result: list[str] = []
+
+		for i in range(0, count):
+			name = NameGenerator.get_name(name_segment_array, starts_with, minimum_length, maximum_length)
+			result.append(name)
+
+		return result
 
 if __name__ == "__main__":
-	# n = NameGenerator()
+	print("*** Random Name Generator ***")
+
 	data = NameGenerator.get_name_data(DefaultNameDataFile)
-	print(data)
+	names = NameGenerator.get_names(data, "?", 3, 10, 1)
+
+	# print(data) # DEBUG:
+	print(names)
